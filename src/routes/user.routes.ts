3579c1b43fb5b69354f6a468b1d52642a2a95e9b
João Souza <ms.joao.felipe@gmail.com>
2025-02-12 09:27:29 -0300

    feat: add user body validation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import { validate } from '../middlewares/validate.middleware';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validation/user.validation';
 
 const router = Router();
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.get('/', userController.getAllUsers);
+router.post('/register', validate(registerUserSchema), userController.register);
+router.post('/login', validate(loginUserSchema), userController.login);
 
 export default router;
---
+++ src/validation/user.validation.ts
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+export const registerUserSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email,
+  password: Joi.string()
+    .regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/)
+    .regex(/[0-9a-zA-Z]*\[a-zA-Z][0-9a-zA-Z]*/)
+    .min(4)
+    .required(),
+});
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().email,
+  password: Joi.string()
+    .regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/)
+    .regex(/[0-9a-zA-Z]*\[a-zA-Z][0-9a-zA-Z]*/)
+    .min(4)
+    .required(),
+});
